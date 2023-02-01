9232cb8e7cc1032a30ec6fcd4a5921d7425d9a09
Erol Demirci <eroldemirci101@gmail.com>
2023-02-01 16:29:36 +0300

    “addedauth”

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from 'aws-amplify';
+
 import {
   Button,
   Flex,
